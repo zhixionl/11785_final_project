da45a5eada6d3df0a45f9099b3031c3fda831186
felicialuo <129685045+felicialuo@users.noreply.github.com>
2023-04-22 23:51:00 -0400

    Update convnext.py

--- models/convnext.py
+++ models/convnext.py
@@ -8,8 +8,8 @@ def convnext_T( pretrained=True, **kwargs):
     # model = torchvision.models.convnext_tiny()
     # config = CNBlockConfig(input_channels=3, out_channels=2048, num_layers)
     if pretrained: 
-            model = torchvision.models.convnext_tiny(num_classes=2048, weights='IMAGENET1K_V1')
-    else: model = torchvision.models.convnext_tiny(num_classes=2048, weights=None)
+            model = torchvision.models.convnext_tiny(weights='IMAGENET1K_V1')
+    else: model = torchvision.models.convnext_tiny(weights=None)
             
     return model
 
