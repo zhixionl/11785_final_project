499b06b02f9d79b00531e19218f6aae245836597
JackLi <114496198+zhixionl@users.noreply.github.com>
2023-05-03 18:40:55 -0400

    Add files via upload
    cleaning code

--- eval_multiview.py
+++ eval_multiview.py
@@ -14,6 +14,7 @@ Running the above command will compute the MPJPE and Reconstruction Error on the
 
 import torch
 import math
+import datetime
 from scipy.spatial.transform import Rotation as R
 
 import gc
@@ -34,6 +35,7 @@ from datasets.base_dataset_multiview import BaseDataset
 #from datasets import BaseDataset
 from utils.imutils import uncrop
 from utils.pose_utils import reconstruction_error
+import pickle
 #from utils.part_utils import PartRenderer
 
 # Define command-line arguments
@@ -62,6 +64,33 @@ def cam_to_world(loc3D, R, T):
 
     return to_return
 
+def average_angle(rot0, rot1):
+    to_return = np.zeros_like(rot0)
+
+    for batch in range(rot0.shape[0]):
+        joints0 = rot0[batch]
+        joints1 = rot1[batch]
+
+        to_return[batch, 0, :, :] = joints0[0, :, :]
+
+        for joint in range(1, 24):
+                    
+            r0 = R.from_matrix(joints0[joint])
+            r1 = R.from_matrix(joints1[joint])
+
+            angles0 = r0.as_euler('xyz', degrees=True)
+            angles1 = r1.as_euler('xyz', degrees=True)
+
+            avg_angles = np.mean([angles0, angles1], axis=0)
+
+            avg_rot = R.from_euler('xyz', avg_angles, degrees=True)
+            avg_matrix = avg_rot.as_matrix()
+
+            to_return[batch, joint, :, :] = avg_matrix
+
+    return np.array(to_return)
+
+
 def Extract_Angle(R):
     sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
     singular = sy < 1e-6
@@ -74,7 +103,7 @@ def Extract_Angle(R):
         x = math.atan2(-R[1, 2], R[1, 1])
         y = math.atan2(-R[2, 0], sy)
         z = 0
-    #return x*180/np.pi, y*180/np.pi, z*180/np.pi
+    #return x*180/np.pi, y*180/np.pi, z*180/np.pi  # if you want to use radio
     return x, y, z
 
 
@@ -106,6 +135,7 @@ def run_evaluation(model, dataset_name, dataset, result_file,
 
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     #data_loader = DataLoader(dataset, batch_size=2, shuffle=shuffle, num_workers=1)
+    
     # Pose metrics
     # MPJPE and Reconstruction error for the non-parametric and parametric shapes
     mpjpe = np.zeros(len(dataset))
@@ -120,21 +150,6 @@ def run_evaluation(model, dataset_name, dataset, result_file,
     # Mean per-vertex error
     shape_err = np.zeros(len(dataset))
     shape_err_smpl = np.zeros(len(dataset))
-
-    # Mask and part metrics
-    # Accuracy
-    accuracy = 0.
-    parts_accuracy = 0.
-    # True positive, false positive and false negative
-    tp = np.zeros((2,1))
-    fp = np.zeros((2,1))
-    fn = np.zeros((2,1))
-    parts_tp = np.zeros((7,1))
-    parts_fp = np.zeros((7,1))
-    parts_fn = np.zeros((7,1))
-    # Pixel count accumulators
-    pixel_count = 0
-    parts_pixel_count = 0
 
     # Store SMPL parameters
     smpl_pose = np.zeros((len(dataset), 72))
@@ -151,17 +166,14 @@ def run_evaluation(model, dataset_name, dataset, result_file,
 
     joint_mapper_h36m = constants.H36M_TO_J17 if dataset_name == 'mpi-inf-3dhp' else constants.H36M_TO_J14
     joint_mapper_gt = constants.J24_TO_J17 if dataset_name == 'mpi-inf-3dhp' else constants.J24_TO_J14
+    
     # Iterate over the entire dataset
-    #print('dataset length:', len(data_loader))
-    # print("###################2############")
-    # print('dataset 2 length: ', len(dataset))
-    # print(dataset[130]['imgname0'])
-    # print(dataset[130]['imgname1'])
+    to_return_vertices = []
     for step, batch in enumerate(tqdm(data_loader, desc='Eval', total=len(data_loader))):
         torch.cuda.empty_cache()
         gc.collect()
 
-        # 
+        # get information from two datasets 
         images_0 = batch['img0'].to(device)
         images_1 = batch['img1'].to(device)
 
@@ -170,7 +182,6 @@ def run_evaluation(model, dataset_name, dataset, result_file,
         
         gt_betas0 = batch['betas0'].to(device)
         gt_betas1 = batch['betas1'].to(device)
-
 
         gt_vertices0 = smpl_neutral(betas=gt_betas0, body_pose=gt_pose0[:, 3:], global_orient=gt_pose0[:, :3]).vertices
         gt_vertices1 = smpl_neutral(betas=gt_betas1, body_pose=gt_pose1[:, 3:], global_orient=gt_pose1[:, :3]).vertices
@@ -181,8 +192,7 @@ def run_evaluation(model, dataset_name, dataset, result_file,
         curr_batch_size0 = images_0.shape[0]
         curr_batch_size1 = images_1.shape[1]
 
-
-        # # Get ground truth annotations from the batch
+        # # Get ground truth annotations from the batch if you have 
         # gt_pose = batch['pose'].to(device)
         # gt_betas = batch['betas'].to(device)
         # gt_vertices = smpl_neutral(betas=gt_betas, body_pose=gt_pose[:, 3:], global_orient=gt_pose[:, :3]).vertices
@@ -190,66 +200,21 @@ def run_evaluation(model, dataset_name, dataset, result_file,
         # gender = batch['gender'].to(device)
         # curr_batch_size = images.shape[0]
 
-    #     print('pass 1')
-        
         with torch.no_grad():
             pred_rotmat0, pred_betas0, pred_camera0 = model(images_0)
             pred_output0 = smpl_neutral(betas=pred_betas0, body_pose=pred_rotmat0[:,1:], global_orient=pred_rotmat0[:,0].unsqueeze(1), pose2rot=False)
             pred_vertices0 = pred_output0.vertices
 
+            to_return_vertices.append(pred_vertices0.cpu())
+
             pred_rotmat1, pred_betas1, pred_camera1 = model(images_1)
             pred_output1 = smpl_neutral(betas=pred_betas1, body_pose=pred_rotmat1[:,1:], global_orient=pred_rotmat1[:,0].unsqueeze(1), pose2rot=False)
             pred_vertices1 = pred_output1.vertices
 
-            
-       # print(pred_rotmat0.shape)
-
-        def average_angle(rot0, rot1):
-            to_return = np.zeros_like(rot0)
-           # print(to_return.shape)
-            for batch in range(rot0.shape[0]):
-                joints0 = rot0[batch]
-                joints1 = rot1[batch]
-
-                to_return[batch, 0, :, :] = joints0[0, :, :]
-
-                for joint in range(1, 24):
-                    #x0, y0, z0, = Extract_Angle(joints0[joint])
-                    #x1, y1, z1, = Extract_Angle(joints1[joint])
-                    #r = R.from_matrix(joints0[joint]).as_rotvec()
-                    #print(r)
-
-                    r0 = R.from_matrix(joints0[joint])
-                    r1 = R.from_matrix(joints1[joint])
-
-                    angles0 = r0.as_euler('xyz', degrees=True)
-                    angles1 = r1.as_euler('xyz', degrees=True)
-
-                    avg_angles = np.mean([angles0, angles1], axis=0)
-
-                    avg_rot = R.from_euler('xyz', avg_angles, degrees=True)
-                    avg_matrix = avg_rot.as_matrix()
-
-                    to_return[batch, joint, :, :] = avg_matrix
-
-            return np.array(to_return)
-
-
-        #print(type(pred_rotmat0))
         avg_rotmat = average_angle(pred_rotmat0.cpu().numpy(), pred_rotmat1.cuda().cpu().numpy())
         pred_avg_rotmat = torch.from_numpy(avg_rotmat).to(device)
         pred_output_avg = smpl_neutral(betas=pred_betas0, body_pose=pred_avg_rotmat[:,1:], global_orient=pred_avg_rotmat[:,0].unsqueeze(1), pose2rot=False)
         pred_vertices_avg = pred_output_avg.vertices
-            
-
-        # if save_results:
-        #     rot_pad = torch.tensor([0,0,1], dtype=torch.float32, device=device).view(1,3,1)
-        #     rotmat = torch.cat((pred_rotmat.view(-1, 3, 3), rot_pad.expand(curr_batch_size * 24, -1, -1)), dim=-1)
-        #     pred_pose = tgm.rotation_matrix_to_angle_axis(rotmat).contiguous().view(-1, 72)
-        #     smpl_pose[step * batch_size:step * batch_size + curr_batch_size, :] = pred_pose.cpu().numpy()
-        #     smpl_betas[step * batch_size:step * batch_size + curr_batch_size, :]  = pred_betas.cpu().numpy()
-        #     smpl_camera[step * batch_size:step * batch_size + curr_batch_size, :]  = pred_camera.cpu().numpy()
-            
     
         # 3D pose evaluation
         if eval_pose:
@@ -262,54 +227,28 @@ def run_evaluation(model, dataset_name, dataset, result_file,
 
                 gt_keypoints_3d_1 = batch['pose_3d1'].cuda()
                 gt_keypoints_3d_1 = gt_keypoints_3d_1[:, joint_mapper_gt, :-1]
-                #gt_keypoints_3d_0 = batch['keypoints3d0'].cuda()
-                #gt_keypoints_3d_1 = batch['keypoints3d1'].cuda()
-                
-                # R0 = batch['R0'].to(device)
-                # R1 = batch['R1'].to(device)
-
-                # T0 = batch['T0'].to(device)
-                # T1 = batch['T1'].to(device)
 
             # Get 14 predicted joints from the mesh
             pred_keypoints_3d_0 = torch.matmul(J_regressor_batch, pred_vertices0)
             pred_keypoints_3d_1 = torch.matmul(J_regressor_batch, pred_vertices1)
             pred_keypoints_3d_avg = torch.matmul(J_regressor_batch, pred_vertices_avg)
             
-            # if save_results:
-            #     pred_joints[step * batch_size:step * batch_size + curr_batch_size, :, :]  = pred_keypoints_3d.cpu().numpy()
             pred_pelvis_0 = pred_keypoints_3d_0[:, [0],:].clone()
             pred_keypoints_3d_0 = pred_keypoints_3d_0[:, joint_mapper_h36m, :] 
             pred_keypoints_3d_0 = pred_keypoints_3d_0 - pred_pelvis_0
 
             gt_keypoints_3d_0 = gt_keypoints_3d_0.squeeze()
-            # gt_keypoints_3d_0 = gt_keypoints_3d_0 / 1000
-            #gt_keypoints_3d_0 = gt_keypoints_3d_0 - gt_keypoints_3d_0[4] # 4 is the root
 
             pred_pelvis_1 = pred_keypoints_3d_1[:, [0],:].clone()
             pred_keypoints_3d_1 = pred_keypoints_3d_1[:, joint_mapper_h36m, :] 
             pred_keypoints_3d_1 = pred_keypoints_3d_1 - pred_pelvis_1
 
-            #
             gt_keypoints_3d_1 = gt_keypoints_3d_1.squeeze()
-            # gt_keypoints_3d_0 = gt_keypoints_3d_0 / 1000
-            #gt_keypoints_3d_1 = gt_keypoints_3d_1 - gt_keypoints_3d_1[4] # 4 is the root
-            #print(pred_keypoints_3d_0.shape)
-
-            #pred_keypoints_3d_0 = cam_to_world(pred_keypoints_3d_0, R0, T0)
-            #pred_keypoints_3d_1 = cam_to_world(pred_keypoints_3d_1, R1, T1)
-
-            # print('predict', pred_keypoints_3d_0)
-            # print('gt', gt_keypoints_3d_0)
 
             pred_pelvis_avg = pred_keypoints_3d_avg[:, [0],:].clone()
             pred_keypoints_3d_avg = pred_keypoints_3d_avg[:, joint_mapper_h36m, :] 
             pred_keypoints_3d_avg = pred_keypoints_3d_avg - pred_pelvis_avg
 
-            
-
-            #print('gt key points 3d ', gt_keypoints_3d_0)
-            #print('pred key points 3d ', pred_keypoints_3d_0)
 
             # Absolute error (MPJPE)
             error0 = torch.sqrt(((pred_keypoints_3d_0 - gt_keypoints_3d_0) ** 2).sum(dim=-1)).mean(dim=-1).cpu().numpy()
@@ -334,10 +273,7 @@ def run_evaluation(model, dataset_name, dataset, result_file,
             recon_err[step * batch_size:step * batch_size + curr_batch_size0] = r_error
             recon_err_avg[step * batch_size:step * batch_size + curr_batch_size0] = r_error_avg
 
-            # print("error: ", error)
-            # print('r_error: ', r_error)
-
-            # exit()
+            # exit()  # if you want to debug the test 
 
         # If mask or part evaluation, render the mask and part images
         #if eval_masks or eval_parts:
@@ -370,6 +306,17 @@ def run_evaluation(model, dataset_name, dataset, result_file,
         print('Reconstruction Error avg: ' + str(1000 * recon_err_avg.mean()))
         print()
 
+
+        timestamp = datetime.datetime.now()
+        checkpoint_filename = os.path.abspath(os.path.join('eval_result', timestamp.strftime('%Y_%m_%d-%H_%M_%S') + '.pt'))
+        np.savez(checkpoint_filename,
+                 MPJPE_0 = 1000 * mpjpe.mean(), 
+                 reconstruction_0 = 1000 * recon_err.mean(),
+                 MPJPE_avg = 1000 * mpjpe_avg.mean(), 
+                 reconstruction_avg = 1000 * recon_err_avg.mean()
+        )
+        
+
 if __name__ == '__main__':
     args = parser.parse_args()
     model = hmr(config.SMPL_MEAN_PARAMS)
@@ -381,10 +328,6 @@ if __name__ == '__main__':
     dataset = BaseDataset(None, args.dataset, is_train=False)
     print("###############################")
     print('dataset 2 length: ', len(dataset))
-    # print(dataset[130]['imgname0'])
-    # print(dataset[130]['imgname1'])
-    # for i in range(100):
-    #     print(dataset.imgname[i])
 
     # Run evaluation
     run_evaluation(model, args.dataset, dataset, args.result_file,
--- train.py
+++ train.py
@@ -1,7 +1,21 @@
 from utils import TrainOptions
 from train import Trainer
+#from train.trainer_spin import Trainer
+import wandb
 
 if __name__ == '__main__':
+
     options = TrainOptions().parse_args()
     trainer = Trainer(options)
+    wandb.login(key="Sorry this is a secret")
+    wandb.init(project="Final Project", entity="zhixionl")
+
+    # Create your wandb run
+    run = wandb.init(
+        #name = "ablation: mlp_5layers_convnext_batchnorm+AdamW", 
+        name = "Convnext 2_layers",
+        reinit = True,
+        project = "Final Project"
+    )
+
     trainer.train()
